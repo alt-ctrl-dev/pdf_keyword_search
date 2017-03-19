d849a04de252820b887607a6b0708904dd9c7896
Reuben <reuben.coutinho@gmail.com>
2017-03-19 14:35:55 +1000

    Chore: initial program.cs commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Nest;
+
+namespace ElasticSearch
+{
+	class MainClass
+	{
+
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World");
+		});
+}
