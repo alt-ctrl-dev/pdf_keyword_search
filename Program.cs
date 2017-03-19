5ba5a656d8ff52b6eea6e34bc0020aa39ec71fb2
Reuben <reuben.coutinho@gmail.com>
2017-03-19 14:38:06 +1000

    Refactor: added code to search directory and read files

--- Program.cs
+++ Program.cs
@@ -11,6 +11,53 @@ namespace ElasticSearch
 
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World");
+			try
+			{
+
+				string roothpath = "/Users/rkc/Projects/pdf_index/";
+
+				if (Directory.Exists(roothpath))
+				{
+					// This path is a directory
+					ProcessDirectory(roothpath);
+				}
+				else
+				{
+					Console.WriteLine("{0} is not a valid file or directory.", roothpath);
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Error: {0}", ex.Message);
+			}
 		});
+		
+		static void ProcessDirectory(string targetDirectory,bool deleteFile=true, string fileType="*.pdf")
+		{
+			Console.WriteLine("Processing directory {0}",targetDirectory);
+			// Process the list of files found in the directory.
+			string[] fileEntries = Directory.GetFiles(targetDirectory, fileType);
+			if (fileEntries.Length <= 0)
+				Console.WriteLine("No files found.");
+			foreach (string fileName in fileEntries)
+				ProcessFile(fileName,deleteFile);
+
+			// Recurse into subdirectories of this directory.
+			string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+			foreach (string subdirectory in subdirectoryEntries)
+				ProcessDirectory(subdirectory,deleteFile);
+
+
+			Console.WriteLine("ProcessDirectory process complete. Proceeding to search");
+		}
+
+		static void ProcessFile(string file, bool deletefile)
+		{
+			Console.WriteLine("Processing file {0}", file);
+
+			//Todo
+
+			if(deletefile)File.Delete(file);
+			Console.WriteLine("File {0} processed", file);
+		}
 }
