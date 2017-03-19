180e3315b78b7b7f7b867a94264376c2927432a3
Reuben <reuben.coutinho@gmail.com>
2017-03-19 14:33:52 +1000

    Chore: Created document type

---
+++ Document.cs
@@ -0,0 +1,12 @@
+﻿using Nest;
+
+namespace ElasticSearch
+{
+	class Document
+	{
+		public string Id { get; set; }
+		public string Path { get; set; }
+		public string Content { get; set; }
+		public Attachment Attachment { get; set; }
+	}
+}
